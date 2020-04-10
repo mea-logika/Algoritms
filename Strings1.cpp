3a616a7fafa0e7ddb91d93fa10f8de960dea73bf
mea-logika <61549605+mea-logika@users.noreply.github.com>
2020-04-10 23:05:02 +0700

    Добавлен алгоритм Раббина Карпа

--- Strings1.cpp
+++ Strings1.cpp
@@ -1,35 +1,105 @@
 #include "TXLib.h"
 
-int StrInStr (const char Ps[], const char Str[]);
-bool IsFind  (const char Ps[], const char Str[], int start);
-int RunTest  ();
+typedef unsigned long long THesh (const char Str[], int LenStr);
+typedef void TSkip (unsigned long long* Value, char CharOut, char CharAdd, unsigned long long HideSeek = 0);
+
+THesh HeshSumm;
+THesh HeshSS;
+
+TSkip SkipHeshSumm;
+TSkip SkipHeshSS;
+
+int RunTest  (THesh*, TSkip*);
 int LenS (const char Str[]);
+bool IsFind  (const char Ps[], const char Str[], int start);
+
+int StrInStr (THesh*, TSkip*, const char Ps[], const char Str[]);
+
+unsigned long long Maska (int len);
 
 int main()
     {
-    RunTest ();
+    RunTest (&HeshSS, &SkipHeshSS);
     }
 
-// Возвращает первое вхождение Подстроки в строке. Если совпадения нет, то возвращает -1
-int StrInStr(const char Ps[], const char Str[])
+// РћРїСЂРµРґРµР»РµРЅРёРµ С…СЌС€ Р·РЅР°С‡РµРЅРёСЏ СЃС‚СЂРѕРєРё. РЎСѓРјРјР° РєРѕРґРѕРІ СЃРёРјРІРѕР»РѕРІ.
+unsigned long long HeshSumm (const char Str[], int LenStr)
+    {
+    int sum = 0;
+    int i = 0;
+    while ((Str[i] != '\0') && (i<LenStr))
+        {
+        sum += Str[i];
+        i++;
+        }
+    return sum;
+    }
+
+void SkipHeshSumm (unsigned long long* Value, char CharOut, char CharAdd, unsigned long long HideSeek)
+    {
+     *Value -= CharOut;
+     *Value += CharAdd;
+    }
+
+void SkipHeshSS   (unsigned long long* Value, char CharOut, char CharAdd, unsigned long long HideSeek)
+   {
+    *Value = (*Value << 8) + CharAdd;
+    *Value = *Value & HideSeek;
+   }
+
+// Р’РѕР·РІСЂР°С‰Р°РµС‚ РїРµСЂРІРѕРµ РІС…РѕР¶РґРµРЅРёРµ РџРѕРґСЃС‚СЂРѕРєРё РІ СЃС‚СЂРѕРєРµ. Р•СЃР»Рё СЃРѕРІРїР°РґРµРЅРёСЏ РЅРµС‚, С‚Рѕ РІРѕР·РІСЂР°С‰Р°РµС‚ -1
+int StrInStr(THesh* Hesh, TSkip* Skip, const char Ps[], const char Str[])
     {
     int i = 0;
     int start = 0;
     int number = -1;
-    while (Str[start] != '\0')
+
+    int LenPs = LenS(Ps);
+    int LenStr = LenS(Str);
+
+    unsigned long long HeshPs = (*Hesh) (Ps, LenPs);
+    unsigned long long HeshInclude = (*Hesh) (Str, LenPs);
+    unsigned long long Hide = Maska (LenPs*8);
+
+    while (start <= LenStr - LenPs)
         {
-        if (IsFind(Ps, Str, start)) return start;
-        else start++;
+        if (HeshPs == HeshInclude)
+            {
+            if (IsFind(Ps, Str, start)) return start;
+                else ;
+            }
+        else
+            {
+            (*Skip) (&HeshInclude, Str[start], Str[start + LenPs], Hide);
+            };
+        start++;
         }
+
     return -1;
     }
 
-// Проверяет совпадает ли подстрока с частью строки, начиная с символа start
+
+// РћРїСЂРµРґРµР»РµРЅРёРµ С…СЌС€ Р·РЅР°С‡РµРЅРёСЏ СЃС‚СЂРѕРєРё. РђРЅР°Р»РѕРі РЎРёСЃС‚РµРј СЃС‡РёСЃР»РµРЅРёСЏ.
+unsigned long long HeshSS (const char Str[], int LenStr)
+    {
+    int result = 0;
+    int i = 0;
+    while ((Str[i] != '\0') && (i<LenStr))
+        {
+        result = result << 8 + Str[i];
+        i++;
+        }
+    return result;
+    }
+
+
+// РџСЂРѕРІРµСЂСЏРµС‚ СЃРѕРІРїР°РґР°РµС‚ Р»Рё РїРѕРґСЃС‚СЂРѕРєР° СЃ С‡Р°СЃС‚СЊСЋ СЃС‚СЂРѕРєРё, РЅР°С‡РёРЅР°СЏ СЃ СЃРёРјРІРѕР»Р° start
 bool IsFind(const char Ps[], const char Str[], int start)
     {
 
     int t = start;
     int d = LenS(Ps);
+
 
     for (int i = 0; i < d; i++)
         {
@@ -42,29 +112,49 @@ bool IsFind(const char Ps[], const char Str[], int start)
 
     }
 
-// Тест работы функции
-int RunTest ()
+// РўРµСЃС‚ СЂР°Р±РѕС‚С‹ С„СѓРЅРєС†РёРё
+int RunTest (THesh* Hesh, TSkip* Skip)
     {
-    $unittest (StrInStr("", "abc"),      0);
-    $unittest (StrInStr("a", "abc"),     0);
-    $unittest (StrInStr("b", "abc"),     1);
-    $unittest (StrInStr("c", "abc"),     2);
 
-    $unittest (StrInStr("bcd", "abcd"),  1);
-    $unittest (StrInStr("bcd", "abct"), -1);
-    $unittest (StrInStr("bcd", "abcd"),  1);
-    $unittest (StrInStr("bcd", "abc"),  -1);
+    $unittest (StrInStr(Hesh, Skip, "", "abc"),      0);
+
+    $unittest (StrInStr(Hesh, Skip,"a", "abc"),     0);
+    $unittest (StrInStr(Hesh, Skip,"b", "abc"),     1);
+    $unittest (StrInStr(Hesh, Skip,"c", "abc"),     2);
+    $unittest (StrInStr(Hesh, Skip,"123","123"),  0);
+
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abcd"),  1);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abct"), -1);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abcd"),  1);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abc"),  -1);
+
+    $unittest (StrInStr(Hesh, Skip,"ABCDEFGH1234567890", "123"),  -1);
+    $unittest (StrInStr(Hesh, Skip,"1","ABCDEFGH1234567890"),  8);
+    $unittest (StrInStr(Hesh, Skip,"123","ABCDEFGH1234567890"),  8);
+
+    $unittest (StrInStr(Hesh, Skip,"123","1234567890"),  0);
+    $unittest (StrInStr(Hesh, Skip,"9","1234567890"),  8);
+    $unittest (StrInStr(Hesh, Skip,"90","1234567890"),  8);
+    $unittest (StrInStr(Hesh, Skip,"901","1234567890"),  -1);
+
+    $unittest (StrInStr(Hesh, Skip,"1234567890","12341234567890"),  4);
 
     }
 
-// Длина строки
+// Р—Р°РїРѕР»РЅСЏРµС‚ РїРѕСЃР»РµРґРЅРёРµ len Р±РёС‚РѕРІ 1
+unsigned long long Maska (int len)
+    {
+    unsigned long long result = 0;
+    for (int i = 1; i <= len; i++) result = (result << 1) + 1;
+    return result;
+    }
+
+// Р”Р»РёРЅР° СЃС‚СЂРѕРєРё
 int LenS (const char Str[])
     {
     int d = 0;
 
     while (Str[d] != '\0') d++;
 
-
     return d;
     }
-
