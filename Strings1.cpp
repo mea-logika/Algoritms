abf02584ca0b0951e24d9f5a86a4c950281cd0da
mea-logika <61549605+mea-logika@users.noreply.github.com>
2020-04-14 01:30:07 +0700

    Update Strings1.cpp

--- Strings1.cpp
+++ Strings1.cpp
@@ -1,7 +1,7 @@
 #include "TXLib.h"
 
-typedef unsigned long long THesh (const char Str[], int LenStr);
-typedef void TSkip (unsigned long long* Value, char CharOut, char CharAdd, unsigned long long HideSeek = 0);
+typedef unsigned long long THesh (const char Str[], int LenStr, int Bits = 4);
+typedef void TSkip (unsigned long long* Value, char CharOut, char CharAdd, int Bits = 4, unsigned long long HideSeek = 0);
 
 THesh HeshSumm;
 THesh HeshSS;
@@ -9,21 +9,24 @@ THesh HeshSS;
 TSkip SkipHeshSumm;
 TSkip SkipHeshSS;
 
-int RunTest  (THesh*, TSkip*);
+int RunTest  (THesh*, TSkip*, int Bits = 4);
 int LenS (const char Str[]);
 bool IsFind  (const char Ps[], const char Str[], int start);
 
-int StrInStr (THesh*, TSkip*, const char Ps[], const char Str[]);
+int StrInStr (THesh*, TSkip*, const char Ps[], const char Str[], int Bits = 4);
 
 unsigned long long Maska (int len);
 
 int main()
     {
-    RunTest (&HeshSS, &SkipHeshSS);
+    int Bits = 8; // количество значимых последних бит одного символа
+    $(Maska(63));
+    $(Maska(64));
+    RunTest (&HeshSS, &SkipHeshSS, Bits);
     }
 
-// РћРїСЂРµРґРµР»РµРЅРёРµ С…СЌС€ Р·РЅР°С‡РµРЅРёСЏ СЃС‚СЂРѕРєРё. РЎСѓРјРјР° РєРѕРґРѕРІ СЃРёРјРІРѕР»РѕРІ.
-unsigned long long HeshSumm (const char Str[], int LenStr)
+// Определение хэш значения строки. Сумма кодов символов.
+unsigned long long HeshSumm (const char Str[], int LenStr, int Bits )
     {
     int sum = 0;
     int i = 0;
@@ -35,20 +38,20 @@ unsigned long long HeshSumm (const char Str[], int LenStr)
     return sum;
     }
 
-void SkipHeshSumm (unsigned long long* Value, char CharOut, char CharAdd, unsigned long long HideSeek)
+void SkipHeshSumm (unsigned long long* Value, char CharOut, char CharAdd, int Bits, unsigned long long HideSeek)
     {
      *Value -= CharOut;
      *Value += CharAdd;
     }
 
-void SkipHeshSS   (unsigned long long* Value, char CharOut, char CharAdd, unsigned long long HideSeek)
+void SkipHeshSS   (unsigned long long* Value, char CharOut, char CharAdd, int Bits, unsigned long long HideSeek)
    {
-    *Value = (*Value << 8) + CharAdd;
+    *Value = (*Value << Bits) + (CharAdd & Maska(Bits));
     *Value = *Value & HideSeek;
    }
 
-// Р’РѕР·РІСЂР°С‰Р°РµС‚ РїРµСЂРІРѕРµ РІС…РѕР¶РґРµРЅРёРµ РџРѕРґСЃС‚СЂРѕРєРё РІ СЃС‚СЂРѕРєРµ. Р•СЃР»Рё СЃРѕРІРїР°РґРµРЅРёСЏ РЅРµС‚, С‚Рѕ РІРѕР·РІСЂР°С‰Р°РµС‚ -1
-int StrInStr(THesh* Hesh, TSkip* Skip, const char Ps[], const char Str[])
+// Возвращает первое вхождение Подстроки в строке. Если совпадения нет, то возвращает -1
+int StrInStr(THesh* Hesh, TSkip* Skip, const char Ps[], const char Str[], int Bits)
     {
     int i = 0;
     int start = 0;
@@ -57,9 +60,10 @@ int StrInStr(THesh* Hesh, TSkip* Skip, const char Ps[], const char Str[])
     int LenPs = LenS(Ps);
     int LenStr = LenS(Str);
 
-    unsigned long long HeshPs = (*Hesh) (Ps, LenPs);
-    unsigned long long HeshInclude = (*Hesh) (Str, LenPs);
-    unsigned long long Hide = Maska (LenPs*8);
+    unsigned long long HeshPs = (*Hesh) (Ps, LenPs, Bits);
+    unsigned long long HeshInclude = (*Hesh) (Str, LenPs, Bits);
+    unsigned long long Hide = Maska (LenPs * Bits);
+    $(Hide);
 
     while (start <= LenStr - LenPs)
         {
@@ -70,7 +74,7 @@ int StrInStr(THesh* Hesh, TSkip* Skip, const char Ps[], const char Str[])
             }
         else
             {
-            (*Skip) (&HeshInclude, Str[start], Str[start + LenPs], Hide);
+            (*Skip) (&HeshInclude, Str[start], Str[start + LenPs], Bits, Hide);
             };
         start++;
         }
@@ -79,21 +83,21 @@ int StrInStr(THesh* Hesh, TSkip* Skip, const char Ps[], const char Str[])
     }
 
 
-// РћРїСЂРµРґРµР»РµРЅРёРµ С…СЌС€ Р·РЅР°С‡РµРЅРёСЏ СЃС‚СЂРѕРєРё. РђРЅР°Р»РѕРі РЎРёСЃС‚РµРј СЃС‡РёСЃР»РµРЅРёСЏ.
-unsigned long long HeshSS (const char Str[], int LenStr)
+// Определение хэш значения строки. Аналог Систем счисления.
+unsigned long long HeshSS (const char Str[], int LenStr, int Bits)
     {
     int result = 0;
     int i = 0;
     while ((Str[i] != '\0') && (i<LenStr))
         {
-        result = result << 8 + Str[i];
+        result = (result << Bits) + (Str[i] & Maska (Bits));
         i++;
         }
     return result;
     }
 
 
-// РџСЂРѕРІРµСЂСЏРµС‚ СЃРѕРІРїР°РґР°РµС‚ Р»Рё РїРѕРґСЃС‚СЂРѕРєР° СЃ С‡Р°СЃС‚СЊСЋ СЃС‚СЂРѕРєРё, РЅР°С‡РёРЅР°СЏ СЃ СЃРёРјРІРѕР»Р° start
+// Проверяет совпадает ли подстрока с частью строки, начиная с символа start
 bool IsFind(const char Ps[], const char Str[], int start)
     {
 
@@ -112,44 +116,43 @@ bool IsFind(const char Ps[], const char Str[], int start)
 
     }
 
-// РўРµСЃС‚ СЂР°Р±РѕС‚С‹ С„СѓРЅРєС†РёРё
-int RunTest (THesh* Hesh, TSkip* Skip)
+// Тест работы функции
+int RunTest (THesh* Hesh, TSkip* Skip, int Bits)
     {
+    /*
+    $unittest (StrInStr(Hesh, Skip, "", "abc", Bits),      0);
 
-    $unittest (StrInStr(Hesh, Skip, "", "abc"),      0);
+    $unittest (StrInStr(Hesh, Skip,"a", "abc", Bits),     0);
+    $unittest (StrInStr(Hesh, Skip,"b", "abc", Bits),     1);
+    $unittest (StrInStr(Hesh, Skip,"c", "abc", Bits),     2);
+    $unittest (StrInStr(Hesh, Skip,"123","123", Bits),  0);
 
-    $unittest (StrInStr(Hesh, Skip,"a", "abc"),     0);
-    $unittest (StrInStr(Hesh, Skip,"b", "abc"),     1);
-    $unittest (StrInStr(Hesh, Skip,"c", "abc"),     2);
-    $unittest (StrInStr(Hesh, Skip,"123","123"),  0);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abcd", Bits),  1);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abct", Bits), -1);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abcd", Bits),  1);
+    $unittest (StrInStr(Hesh, Skip,"bcd", "abc", Bits),  -1);
 
-    $unittest (StrInStr(Hesh, Skip,"bcd", "abcd"),  1);
-    $unittest (StrInStr(Hesh, Skip,"bcd", "abct"), -1);
-    $unittest (StrInStr(Hesh, Skip,"bcd", "abcd"),  1);
-    $unittest (StrInStr(Hesh, Skip,"bcd", "abc"),  -1);
+    $unittest (StrInStr(Hesh, Skip,"ABCDEFGH1234567890", "123", Bits),  -1);
+    $unittest (StrInStr(Hesh, Skip,"1","ABCDEFGH1234567890", Bits),  8);
+    $unittest (StrInStr(Hesh, Skip,"123","ABCDEFGH1234567890", Bits),  8);
 
-    $unittest (StrInStr(Hesh, Skip,"ABCDEFGH1234567890", "123"),  -1);
-    $unittest (StrInStr(Hesh, Skip,"1","ABCDEFGH1234567890"),  8);
-    $unittest (StrInStr(Hesh, Skip,"123","ABCDEFGH1234567890"),  8);
-
-    $unittest (StrInStr(Hesh, Skip,"123","1234567890"),  0);
-    $unittest (StrInStr(Hesh, Skip,"9","1234567890"),  8);
-    $unittest (StrInStr(Hesh, Skip,"90","1234567890"),  8);
-    $unittest (StrInStr(Hesh, Skip,"901","1234567890"),  -1);
-
-    $unittest (StrInStr(Hesh, Skip,"1234567890","12341234567890"),  4);
+    $unittest (StrInStr(Hesh, Skip,"123","1234567890", Bits),  0);
+    $unittest (StrInStr(Hesh, Skip,"9","1234567890", Bits),  8);
+    $unittest (StrInStr(Hesh, Skip,"90","1234567890", Bits),  8);
+    $unittest (StrInStr(Hesh, Skip,"901","1234567890", Bits),  -1);
+    */
+    $unittest (StrInStr(Hesh, Skip,"1234567890","12341234567890", Bits),  4);
 
     }
 
-// Р—Р°РїРѕР»РЅСЏРµС‚ РїРѕСЃР»РµРґРЅРёРµ len Р±РёС‚РѕРІ 1
+// Заполняет последние len битов 1
 unsigned long long Maska (int len)
     {
-    unsigned long long result = 0;
-    for (int i = 1; i <= len; i++) result = (result << 1) + 1;
-    return result;
+    if (len < 64) return (1 << len) - 1;
+        else return (((1 << 63) - 1) << 1) + 1;
     }
 
-// Р”Р»РёРЅР° СЃС‚СЂРѕРєРё
+// Длина строки
 int LenS (const char Str[])
     {
     int d = 0;
@@ -158,3 +161,4 @@ int LenS (const char Str[])
 
     return d;
     }
+
